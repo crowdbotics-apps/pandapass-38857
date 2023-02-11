981674f2486a041d55caa7da03918ca39b2115e4
Crowdbotics <team@crowdbotics.com>
2023-02-11 08:55:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pandapass-38857",
+  url: "https://pandapass-38857.botics.co",
+  api: "https://pandapass-38857.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
